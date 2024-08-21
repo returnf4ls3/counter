258e6f3884e5f2619096350a8afcc489785ba7aa
return_f4ls3 <85391121+returnf4ls3@users.noreply.github.com>
2024-08-21 14:27:37 +0900

    Update route.ts

--- app/api/count/[targetId]/route.ts
+++ app/api/count/[targetId]/route.ts
@@ -22,7 +22,7 @@ export async function GET(
     return NextResponse.json(count);
 }
 
-export async function POST(
+export async function PATCH(
     request: Request,
     { params }: { params: IParams }
 ) {
